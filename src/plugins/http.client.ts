c5c14ed653918fdc4bec84b8dae54367295fabaf
kungbi <kipper12335@gmail.com>
2025-05-23 17:02:28 +0900

    feat(http.client.ts): HTTP 클라이언트 플러그인 추가

---
+++ src/plugins/http.client.ts
@@ -0,0 +1,56 @@
+import got, { ExtendOptions, Got } from 'got';
+import type FormData from 'form-data';
+
+interface BaseRequestOptions {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+  url: string;
+  queryParams?: Record<string, string | number>;
+  headers?: Record<string, string>;
+  throwHttpErrors?: boolean;
+}
+
+interface JsonRequestOptions<T extends object = object> extends BaseRequestOptions {
+  body?: T;
+}
+
+interface FormRequestOptions extends BaseRequestOptions {
+  body: FormData;
+}
+
+export class GotClient {
+  client: Got;
+
+  constructor(options?: ExtendOptions) {
+    this.client = got.extend({
+      responseType: 'json',
+      timeout: { request: 300 },
+      ...options,
+    });
+  }
+
+  async requestForm<T>(options: FormRequestOptions) {
+    return this.client<T>(options.url, {
+      method: options.method,
+      body: options.body,
+      searchParams: options.queryParams,
+      headers: options.headers,
+      throwHttpErrors: options.throwHttpErrors ?? false,
+    });
+  }
+
+  async requestJson<T>(options: JsonRequestOptions) {
+    return this.client<T>(options.url, {
+      method: options.method,
+      json: options.body,
+      searchParams: options.queryParams,
+      headers: options.headers,
+      throwHttpErrors: options.throwHttpErrors ?? false,
+    });
+  }
+}
+
+export const gotClient = new GotClient({
+  get throwHttpErrors() {
+    return false;
+  },
+});
