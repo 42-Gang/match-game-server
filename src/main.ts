bbc5c5c3b73a550a5613a81cc6f346047df8f738
kungbi <kipper12335@gmail.com>
2025-05-23 15:38:20 +0900

    feat(main.ts): graceful shutdown 구현

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createServer, Server } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import { v4 as uuid } from 'uuid';
 import { redis } from './plugins/redis.js';
+import closeWithGrace from 'close-with-grace';
 
 const serverId = uuid(); // 이 서버의 고유 ID
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,7 @@ function registerSocketServer() {
     console.log('User connected to match-server:', serverId);
     // 게임 로직 처리
   });
-  return httpServer;
+  return { httpServer, io };
 }
 
 function startServer(server: Server) {
@@ -35,18 +36,31 @@ async function configureServer() {
   await redis.sAdd('match-server:active', serverId);
 }
 
-async function init() {
-  const server = registerSocketServer();
-  await configureServer();
-  startServer(server);
+export async function setupGracefulShutdown(server: Server, socket: SocketIOServer) {
+  closeWithGrace(
+    {
+      delay: Number(process.env.CLOSE_GRACE_PERIOD) || 500,
+    },
+    async ({ err }) => {
+      if (err != null) {
+        console.log(err);
+      }
 
-  // 서버 종료 시 Redis에서 정보 제거
-  process.on('SIGINT', async () => {
-    await redis.sRem('match-server:active', serverId);
-    await redis.del(`match-server:${serverId}`);
-    await redis.quit();
-    process.exit();
-  });
+      server.close();
+      socket.close();
+
+      await redis.sRem('match-server:active', serverId);
+      await redis.del(`match-server:${serverId}`);
+      await redis.quit();
+    },
+  );
+}
+
+async function init() {
+  const { httpServer, io } = registerSocketServer();
+  await configureServer();
+  startServer(httpServer);
+  await setupGracefulShutdown(httpServer, io); // 서버 종료 시그널 핸들러 등록
 }
 
 init();
