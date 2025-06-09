ced58c549554d598f7c5c89517f7441271a08934
kungbi <kipper12335@gmail.com>
2025-06-09 19:04:25 +0900

    refactor(GameSession.spec.ts): beforeEach로 테스트 설정 분리

--- test/domain/GameSession.spec.ts
+++ test/domain/GameSession.spec.ts
@@ -6,19 +6,23 @@ import Table from '../../src/domain/physics/Table.js';
 import Racket from '../../src/domain/physics/Racket.js';
 import Judgement from '../../src/domain/Judgement.js';
 import Scores from '../../src/domain/Scores.js';
-import { it } from 'vitest';
+import { beforeEach, it } from 'vitest';
 
-const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
-const ball = new Ball();
-const table = new Table();
-const racket1 = new Racket(1, new CANNON.Vec3(-0.5, 0.2, 0));
-const racket2 = new Racket(2, new CANNON.Vec3(0.5, 0.2, 0));
+beforeEach(() => {
+  const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
+  const ball = new Ball();
+  const table = new Table();
+  const racket1 = new Racket(1, new CANNON.Vec3(-0.5, 0.2, 0));
+  const racket2 = new Racket(2, new CANNON.Vec3(0.5, 0.2, 0));
 
-const physicsEngine = new PhysicsEngine(world, ball, table, racket1, racket2);
-const scores = new Scores();
-const judgement = new Judgement(scores);
+  const physicsEngine = new PhysicsEngine(world, ball, table, racket1, racket2);
+  const scores = new Scores();
+  const judgement = new Judgement(scores);
 
-const gameSession = new GameSession(1, physicsEngine, 1, 2, judgement);
+  gameSession = new GameSession(1, physicsEngine, 1, 2, judgement);
+});
+
+let gameSession: GameSession;
 
 it('should ', () => {
   console.log(gameSession.getState());
