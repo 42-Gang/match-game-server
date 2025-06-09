5994ab7a833ddae98fe2d82c12e39e02f4670f7f
kungbi <kipper12335@gmail.com>
2025-06-09 17:36:39 +0900

    refactor(Table.ts): 테이블 크기 속성 추가 및 초기화

--- src/domain/physics/Table.ts
+++ src/domain/physics/Table.ts
@@ -2,11 +2,16 @@ import * as CANNON from 'cannon-es';
 import PhysicsEngine from './PhysicsEngine.js';
 
 export default class Table {
-  constructor(engine: PhysicsEngine) {
+  constructor(
+    private readonly engine: PhysicsEngine,
+    private readonly x: number = 1.37, // 1370mm
+    private readonly y: number = 0.015, // 15mm
+    private readonly z: number = 2.74, // 2740mm
+  ) {
     const material = new CANNON.Material('tableMaterial');
     const table = new CANNON.Body({
       type: CANNON.Body.STATIC,
-      shape: new CANNON.Box(new CANNON.Vec3(1.37, 0.015, 2.74)), // 2740×1525mm
+      shape: new CANNON.Box(new CANNON.Vec3(x, y, z)), // 2740×1525mm
       material,
     });
     table.position.set(0, 0, 0);
