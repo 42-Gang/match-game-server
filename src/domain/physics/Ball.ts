c9f199941e1f175e6d88a529e815712730ae49a2
kungbi <kipper12335@gmail.com>
2025-06-10 14:02:25 +0900

    feat(Ball.ts): 공에 마지막으로 친 플레이어 정보 추가

--- src/domain/physics/Ball.ts
+++ src/domain/physics/Ball.ts
@@ -1,7 +1,10 @@
 import * as CANNON from 'cannon-es';
+import { PlayerType } from '../game.schema.js';
 
 export default class Ball {
   public body: CANNON.Body;
+  private lastHitPlayer: PlayerType | null = null;
+  private touchedTable: boolean = false;
 
   constructor() {
     const material = new CANNON.Material('ballMaterial');
@@ -17,5 +20,24 @@ export default class Ball {
     this.body.position.set(0, 0.2, 0);
     this.body.velocity.set(0, 0, 0);
     this.body.angularVelocity.set(0, 0, 0);
+
+    this.lastHitPlayer = null;
+    this.touchedTable = false;
+  }
+
+  setLastHitPlayer(player: PlayerType): void {
+    this.lastHitPlayer = player;
+  }
+
+  getLastHitPlayer(): PlayerType | null {
+    return this.lastHitPlayer;
+  }
+
+  markTableTouched(): void {
+    this.touchedTable = true;
+  }
+
+  hasTouchedTable(): boolean {
+    return this.touchedTable;
   }
 }
