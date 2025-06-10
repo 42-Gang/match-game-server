1bc08b4c338ebec3338f7eaa1e1ff5e353dd53a4
kungbi <kipper12335@gmail.com>
2025-06-10 20:10:16 +0900

    feat: 소켓 통신 설정 및 간단 게임 상태 전달 구현

--- src/network/socket.ts
+++ src/network/socket.ts
@@ -1,11 +1,44 @@
 import { Server } from 'socket.io';
 import { asValue, AwilixContainer } from 'awilix';
 import { socketMiddleware } from './utils/middleware.js';
+import Ball from '../domain/physics/Ball.js';
+import Table from '../domain/physics/Table.js';
+import Racket from '../domain/physics/Racket.js';
+import CANNON from 'cannon-es';
+import GameSpace from '../domain/physics/GameSpace.js';
 
 export const registerSocket = (diContainer: AwilixContainer, io: Server) => {
   io.use(socketMiddleware);
 
   diContainer.register({
     socket: asValue(io),
+  });
+
+  io.on('connection', (socket) => {
+    const logger = io.logger;
+    logger.info(`New connection: ${socket.id} from user ${socket.data.userId}`);
+
+    const ball = new Ball();
+    const table = new Table();
+    const racket1 = new Racket(1, new CANNON.Vec3(-0.5, 0.2, 0));
+    const racket2 = new Racket(2, new CANNON.Vec3(0.5, 0.2, 0));
+    const gameSpace = new GameSpace(ball, table, racket1, racket2);
+
+    const fixedTimeStep = 1 / 60; // 60Hz
+    const intervalMs = fixedTimeStep * 1000;
+
+    const timer = setInterval(() => {
+      gameSpace.step(fixedTimeStep);
+
+      socket.emit('game:update', {
+        ball: gameSpace.getBallPosition(),
+      });
+
+      // 공이 바닥에 닿으면 루프 종료
+      if (ball.body.position.y <= 0) {
+        clearInterval(timer);
+        // 필요하면 여기서 플레이어에게 결과 전송
+      }
+    }, intervalMs);
   });
 };
