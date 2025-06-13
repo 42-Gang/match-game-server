201cec9764f11441c826378d000c15f7beae4ab8
kungbi <kipper12335@gmail.com>
2025-06-13 21:51:23 +0900

    refactor: GameSession으로 게임 인스턴스 관리 로직 분리 및 관리

--- src/domain/GameSession.ts
+++ src/domain/GameSession.ts
@@ -1,30 +1,65 @@
 import GameSpace from './physics/GameSpace.js';
-import { ScoreDto, SessionStateType } from './game.schema.js';
-import Players from './Players.js';
+import { playerTypeSchema } from './game.schema.js';
+import Ball from './physics/Ball.js';
+import Table from './physics/Table.js';
+import Racket from './physics/Racket.js';
+import { Server } from 'socket.io';
 
 export default class GameSession {
-  private readonly dt = 1 / 60;
+  private readonly gameSpaces: Map<number, GameSpace>;
 
-  constructor(
-    private readonly matchId: number,
-    private readonly engine: GameSpace,
-    private readonly players: Players,
-  ) {}
+  constructor(private readonly io: Server) {
+    this.gameSpaces = new Map<number, GameSpace>();
 
-  tick(): void {
-    // 물리 엔진을 한 스텝 진행합니다.
-    this.engine.step(this.dt);
+    this.startLoop();
   }
 
-  getState(): SessionStateType {
-    return {
-      ball: this.engine.getBallPosition(),
-      racket1: this.engine.getRacket1Position(),
-      racket2: this.engine.getRacket2Position(),
-    };
+  private startLoop() {
+    const fixedTimeStep = 1 / 60; // 60Hz
+    const intervalMs = fixedTimeStep * 1000;
+
+    const timer = setInterval(() => {
+      for (const [matchId, gameSpace] of this.gameSpaces.entries()) {
+        gameSpace.step(fixedTimeStep);
+
+        const message = {
+          ball: gameSpace.getBallPosition(),
+          racket1: gameSpace.getRacket1Position(),
+          racket2: gameSpace.getRacket2Position(),
+        };
+        this.io.to(`match:${matchId}`).emit('game:update', message);
+
+        if (gameSpace.getBallPosition().y <= 0) {
+          clearInterval(timer);
+        }
+      }
+    }, intervalMs);
   }
 
-  getMatchId(): number {
-    return this.matchId;
+  createGameSpace(input: { matchId: number; player1Id: number; player2Id: number }) {
+    if (this.isExist(input.matchId)) {
+      throw new Error(`Game space for match ID ${input.matchId} already exists.`);
+    }
+
+    const ball = new Ball();
+    const table = new Table();
+    const racket1 = new Racket(playerTypeSchema.enum.PLAYER1);
+    const racket2 = new Racket(playerTypeSchema.enum.PLAYER2);
+    const gameSpace = new GameSpace(ball, table, racket1, racket2, input.player1Id);
+
+    this.gameSpaces.set(input.matchId, gameSpace);
+  }
+
+  updateRacketPosition(matchId: number, playerId: number, x: number, y: number, z: number) {
+    const gameSpace = this.gameSpaces.get(matchId);
+    if (!gameSpace) {
+      throw new Error(`Game space for match ID ${matchId} not found.`);
+    }
+
+    gameSpace.updateRacketPosition(playerId, x, y, z);
+  }
+
+  isExist(matchId: number): boolean {
+    return this.gameSpaces.has(matchId);
   }
 }
--- src/network/socket.ts
+++ src/network/socket.ts
@@ -1,20 +1,8 @@
 import { Server } from 'socket.io';
 import { asValue, AwilixContainer } from 'awilix';
 import { socketMiddleware } from './utils/middleware.js';
-import Ball from '../domain/physics/Ball.js';
-import Table from '../domain/physics/Table.js';
-import Racket from '../domain/physics/Racket.js';
-import GameSpace from '../domain/physics/GameSpace.js';
-import { playerTypeSchema } from '../domain/game.schema.js';
 import { matchMiddleware } from './match.middleware.js';
-
-function createGameSpace(playerId: number) {
-  const ball = new Ball();
-  const table = new Table();
-  const racket1 = new Racket(playerTypeSchema.enum.PLAYER1);
-  const racket2 = new Racket(playerTypeSchema.enum.PLAYER2);
-  return new GameSpace(ball, table, racket1, racket2, playerId);
-}
+import GameSession from '../domain/GameSession.js';
 
 export const registerSocket = (diContainer: AwilixContainer, io: Server) => {
   io.use(socketMiddleware);
@@ -24,40 +12,7 @@ export const registerSocket = (diContainer: AwilixContainer, io: Server) => {
     io: asValue(io),
   });
 
-  const fixedTimeStep = 1 / 60; // 60Hz
-  const intervalMs = fixedTimeStep * 1000;
-
-  const matches = new Map<number, GameSpace>();
-  const timer = setInterval(() => {
-    for (const [matchId, gameSpace] of matches.entries()) {
-      gameSpace.step(fixedTimeStep);
-
-      const message = {
-        ball: gameSpace.getBallPosition(),
-        racket1: gameSpace.getRacket1Position(),
-        racket2: gameSpace.getRacket2Position(),
-      };
-      io.to(`match:${matchId}`).emit('game:update', message);
-
-      if (gameSpace.getBallPosition().y <= 0) {
-        clearInterval(timer);
-      }
-    }
-  }, intervalMs);
-
-  function joinMatch(matchId: number, playerId: number) {
-    if (!matches.has(matchId)) {
-      matches.set(matchId, createGameSpace(playerId));
-      return;
-    }
-
-    const match = matches.get(matchId);
-    if (!match) {
-      io.logger.error(`Match not found for ID ${matchId}`);
-      return;
-    }
-    match.setPlayer2Id(playerId);
-  }
+  const gameSession = new GameSession(io);
 
   io.on('connection', (socket) => {
     const logger = io.logger;
@@ -67,19 +22,9 @@ export const registerSocket = (diContainer: AwilixContainer, io: Server) => {
     socket.join(`match:${matchId}`);
     logger.info(`New connection: ${socket.id} from user ${socket.data.userId}`);
 
-    joinMatch(matchId, playerId);
-
     socket.on('racket:update', (data) => {
       const { x, y, z } = data;
-      const gameSpace = matches.get(matchId);
-      if (!gameSpace) {
-        logger.error(`Game space not found for match ID ${matchId}`);
-        return;
-      }
-
-      gameSpace.updateRacketPosition(playerId, x, y, z);
-      if (playerId === 222)
-        console.log(`User ${playerId} racket update received: x=${x}, y=${y}, z=${z}`);
+      gameSession.updateRacketPosition(matchId, playerId, x, y, z);
     });
   });
 };
