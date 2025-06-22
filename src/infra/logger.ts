9d8d4d01459ed7bea9fa10ef2751ccd5206cb69a
kungbi <kipper12335@gmail.com>
2025-06-22 13:29:32 +0900

    chore(logger.ts): Pino 로거 레벨을 debug로 설정

--- src/infra/logger.ts
+++ src/infra/logger.ts
@@ -3,6 +3,7 @@ import { pino } from 'pino';
 export const logger = pino({
   transport: {
     target: 'pino-pretty',
+    level: 'debug',
     options: {
       colorize: true,
       translateTime: 'yyyy-mm-dd HH:MM:ss',
