96bc0ff6667e06e1f4bdd6f730372d9b71f8f66a
kungbi <kipper12335@gmail.com>
2025-06-22 21:19:38 +0900

    refactor(GameSession.ts): 로깅 레벨을 info에서 debug로 변경

--- src/infra/logger.ts
+++ src/infra/logger.ts
@@ -1,13 +1,15 @@
 import { pino } from 'pino';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
+export const logger = pino(
+  {
     level: 'debug',
+  },
+  pino.transport({
+    target: 'pino-pretty',
     options: {
       colorize: true,
       translateTime: 'yyyy-mm-dd HH:MM:ss',
       ignore: 'pid,hostname',
     },
-  },
-});
+  }),
+);
